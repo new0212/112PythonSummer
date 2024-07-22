a42d97a546d518a78b65ea10f3342b311dbf1360
new0212 <a110060302@mail.shu.edu.tw>
2024-07-22 16:01:57 +0800

    113-07-22

---
+++ 113-07-22/02-Karel/karel_11.py
@@ -0,0 +1,13 @@
+from stanfordkarel import *
+
+def main():
+    """ Karel code goes here! """
+    from stanfordkarel.world_editor import run_world_editor 
+    
+    if __name__ == "__main__": 
+        run_world_editor()
+
+    pass
+
+if __name__ == "__main__":
+    run_karel_program('collect_newspaper_karel')
